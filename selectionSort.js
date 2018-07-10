25536cdec4d8a76ea749c2e7c96a248c29600ebd
talkasrul <talkasrul@gmail.com>
2018-07-10 15:22:18 +0700

    Mengisi Kode Selection Sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,1 +1,21 @@
 // your code here
+function selectionSort(arr) {
+    var hasil = [];
+    while(arr.length > 0) {
+        var idx = 0;
+        var palingKecil = arr[0];
+        for(let i = 0; i < arr.length; i++) {
+            if(palingKecil > arr[i]) {
+                palingKecil = arr[i];
+                idx = i;
+            }
+        }
+        hasil.push(palingKecil);
+        arr.splice(idx, 1)
+    }
+    
+    return hasil;
+}
+
+console.log(selectionSort([33, 2, 52, 106, 73]));
+console.log(selectionSort([13, 5, 22, 99, 11]));
