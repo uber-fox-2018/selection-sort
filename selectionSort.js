079fd62553e244d0114e9f8194759ff245c82f77
wisnugautama <ngurahwisnugautama@gmail.com>
2018-07-10 17:36:32 +0700

    done

--- selectionSort.js
+++ selectionSort.js
@@ -1,1 +1,20 @@
 // your code here
+function selectionSort(array) {
+    let output = []
+    while (array.length !== 0) {
+        let index = 0
+        let min = array[0]
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i]
+                index = i
+            }
+        }
+        output.push(min)
+        array.splice(index,1)
+    }
+    return output
+}
+
+console.log(selectionSort([33,2,52,106,7]));
+console.log(selectionSort([13,5,22,99,11]));
