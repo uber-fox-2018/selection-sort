8493b1c1cd2fe573f1cf3255f69f0b77818e2d7b
minzarddillah <minzard.dillah@gmail.com>
2018-07-10 19:10:31 +0700

    selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,14 +1,18 @@
 function selectionSort(arr){
-    for(var i = 1 ; i < arr.length ; i++){
-        for(var j = 0 ; j < i ; j++){
-            if(arr[i] < arr[j]){
-                var tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
+    for(var i = 0 ; i < arr.length ; i++){
+        var tmpArr = arr[i] // 3
+        var tmpPmr = i // 0
+        for(var j = i ; j < arr.length ; j++){
+            if(tmpArr > arr[j]){
+                tmpArr = arr[j] // 2
+                tmpPmr = j // 9
             }
         }
+        var tmp = arr[i]
+        arr[i] = tmpArr
+        arr[tmpPmr] = tmp
     }
     return arr
 }
-console.log(selectionSort([ 33, 2, 52, 106, 73 ]))  // [ 2, 33, 52, 73, 106 ]
+console.log(selectionSort([ 3, 44, 38 ,5 ,47, 15, 36 ,26 ,27, 2, 46 ]))
 console.log(selectionSort([ 13, 5, 22, 99, 11]))    // [ 5, 11, 13, 22, 99 ]
