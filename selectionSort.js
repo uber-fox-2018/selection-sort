fe4d824b11745940a0f71fef72bfe55a88f4b649
minzarddillah <minzard.dillah@gmail.com>
2018-07-10 16:25:31 +0700

    selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,1 +1,14 @@
-// your code here
+function selectionSort(arr){
+    for(var i = 1 ; i < arr.length ; i++){
+        for(var j = 0 ; j < i ; j++){
+            if(arr[i] < arr[j]){
+                var tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+            }
+        }
+    }
+    return arr
+}
+console.log(selectionSort([ 33, 2, 52, 106, 73 ]))  // [ 2, 33, 52, 73, 106 ]
+console.log(selectionSort([ 13, 5, 22, 99, 11]))    // [ 5, 11, 13, 22, 99 ]
